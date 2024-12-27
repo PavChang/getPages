715e77e59de6462e5fc0affb81b73636ff76198b
tongjian <zhtongjian@gmail.com>
2024-12-27 13:15:04 +0100

    debug

--- frontend/app.js
+++ frontend/app.js
@@ -5,13 +5,20 @@ let responses = document.querySelector("#responses")
 
 form.addEventListener("submit", async (e) => {
   e.preventDefault()
-  var python = require('child_process').spawn('python', ['/backend/backend.py', address.value, ip.value]);
+  
+  var python = require('child_process').spawn('/Users/tongjian/Documents/app/getPages/backend/bin/python', ['-u', 'backend/backend.py', address.value, ip.value]);
   python.stdout.on('data',function(data){
       console.log("data: ",data.toString('utf8'));
+      let responseText = data.toString('utf8')+address.value+ip.value
+      let response = document.createElement("div")
+      response.textContent = responseText
+      responses.appendChild(response)
   });
-  let responseText = data.toString('utf8')+address.value+ip.value
+  python.stderr.on('data',function(data){
+    console.log("err data: ",data.toString('utf8'));
+  });
+  python.on('close',(code) => {
+    console.log(`child process exited with code ${code}`);
+  });
   
-  let response = document.createElement("div")
-  response.textContent = responseText
-  responses.appendChild(response)
 })
--- frontend/main.js
+++ frontend/main.js
@@ -5,10 +5,14 @@ const { app, BrowserWindow } = require('electron')
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
-    height: 600
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+  }
   })
-
   win.loadFile('index.html')
+  win.webContents.openDevTools()
 }
 
 app.on("ready", createWindow)
